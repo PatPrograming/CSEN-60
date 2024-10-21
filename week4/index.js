ed31c427ec43e0357cc1ede7914bec79d09f17d2
PatPrograming <30586247+PatPrograming@users.noreply.github.com>
2024-10-21 11:55:06 -0700

    Week 5 js cont.

--- week4/index.js
+++ week4/index.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+// console.log("hello world")
+// alert('Hello, world!');
+window.document.write('<p>Welcome to CSEN 60!</p>');
+
+// document.append('<p>This should come after the h1</p>')
+
+/*
+    Function to calculate the sum of two numbers.
+    Inputs: a, b - numbers to be added.
+    Output: returns the sum of a and b.
+*/
+// TODO fix this function
+
+/**
+ * @description this function adds two numbers
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns
+ */
+function add(a, b) {
+    return a + b;
+}
